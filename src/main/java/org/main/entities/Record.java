49600986fb318003c552723b74a3a47cc98475cb
AlinaZabolotnaia <zabolotnaialina@gmail.com>
2026-05-26 19:15:38 +0300

    fixes

--- src/main/java/org/main/entities/Record.java
+++ src/main/java/org/main/entities/Record.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 public class Record {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "date")
